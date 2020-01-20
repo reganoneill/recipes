9d853afa850bcb92b5705b702f0dd2ed10301bf4
regan <rkrotho@gmail.com>
2020-01-20 11:43:28 -0800

    adds footer and site wide formatting

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,18 @@ const App = () => {
             <RecipePage path="/recipes/:recipe" />
             <PrivateRoute path="/admin" component={"Admin"} />
           </Router>
+          <footer className="footer">
+            <p>
+              created by{" "}
+              <a href="https://github.com/reganoneill" target="_blank">
+                regan oneill
+              </a>{" "}
+              with culinary support from{" "}
+              <a href="https://talorae.com/" target="_blank">
+                rae
+              </a>
+            </p>
+          </footer>
         </div>
       </AuthContext.Provider>
     </Provider>
